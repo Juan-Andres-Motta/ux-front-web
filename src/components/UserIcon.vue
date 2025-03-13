9082c2d7268b5b847d604dc4239ef13e620930f0
Juan Andres Motta <ja.lopezm123@uniandes.edu.co>
2025-03-12 21:57:09 -0500

    add ReportIcon and UserIcon components with SVG templates

---
+++ src/components/UserIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="width" :height="height" viewBox="0 0 24 24" fill="none"
+    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+    class="icon icon-tabler icons-tabler-outline icon-tabler-user">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+    <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  width: {
+    type: Number,
+    default: 24
+  },
+  height: {
+    type: Number,
+    default: 24
+  }
+})
+</script>
